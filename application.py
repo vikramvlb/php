d66b30f7de59ad03331697b0e2fc16f6c71903d4
vikram <40787172+vikramvlb@users.noreply.github.com>
2018-10-30 14:21:44 +0530

    update

--- application.py
+++ application.py
@@ -111,7 +111,7 @@ welcome = """
 <body id="sample">
   <div class="textColumn">
     <h1>Congratulations</h1>
-    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <p> vikram your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
   <div class="linksColumn"> 
